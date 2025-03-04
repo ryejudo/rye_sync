dccf579fbac21af310797c563606ca7e6e757a5f
ryejudo <69042135+ryejudo@users.noreply.github.com>
2025-03-04 13:43:48 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version      'cerulean'
+game            'gta5'
+name            'rye_sync'
+lua54           'yes'
+author          'ryejudo'
+description     'Sync Headshots to Oneshot'
+version         '1.0'
+
+
+server_scripts {
+    'serverside/sv_sync.lua'
+}
+
+client_scripts {
+    'clientside//cl_sync.lua',
+}
+
+
